0804602fbc55a9c7f9224a434cf670816df44c4c
aldorQlok <aldor.besher@qlok.se>
2026-03-19 11:21:25 +0100

    updated Route name

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import NavBar from './components/NavBar'
 function App() {
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/ReactRouterDemo'>
         <NavBar />
         <Routes>
           <Route path='/' element={<Home />} />
